95fe117372803961a104f79904aba88405827307
junwei17 <77657709+junwei17@users.noreply.github.com>
2022-12-13 01:41:44 +0800

    Updates

---
+++ precache-manifest.c27d7f8c548cb8301ddfa3645af2d18d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f492517f6a262fe2f7989306e16bac9e",
+    "url": "/resume-website/index.html"
+  },
+  {
+    "revision": "7cd472930269c9450ef2",
+    "url": "/resume-website/static/css/main.99e75adc.chunk.css"
+  },
+  {
+    "revision": "181581d9e5281965d770",
+    "url": "/resume-website/static/js/2.028d8fa1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/resume-website/static/js/2.028d8fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd472930269c9450ef2",
+    "url": "/resume-website/static/js/main.323105ce.chunk.js"
+  },
+  {
+    "revision": "9dafde0836abcad01bf8",
+    "url": "/resume-website/static/js/runtime-main.741b4677.js"
+  },
+  {
+    "revision": "a584f07ccc08b8ae5b3ee10871e5775f",
+    "url": "/resume-website/static/media/code2.a584f07c.svg"
+  },
+  {
+    "revision": "5912e88546ca137ba65a15c17d778a69",
+    "url": "/resume-website/static/media/heart.5912e885.svg"
+  },
+  {
+    "revision": "213b76969264dc33031a8ed3f76fe65f",
+    "url": "/resume-website/static/media/hobbies.213b7696.svg"
+  },
+  {
+    "revision": "ae38ec0e536425514da8193de24ef754",
+    "url": "/resume-website/static/media/img2-modified.ae38ec0e.png"
+  },
+  {
+    "revision": "3d54d07e505d881b0f04ee9cc51a6285",
+    "url": "/resume-website/static/media/ootb.3d54d07e.png"
+  },
+  {
+    "revision": "78920d803d1f55b68b3dd58e2fbb04fc",
+    "url": "/resume-website/static/media/tgt.78920d80.png"
+  },
+  {
+    "revision": "bfd5f2d3f8b1f28b88b83d3cc7d75d55",
+    "url": "/resume-website/static/media/tsi.bfd5f2d3.png"
+  },
+  {
+    "revision": "878a6941afc06498989a52d2efbe9dba",
+    "url": "/resume-website/static/media/uob.878a6941.png"
+  },
+  {
+    "revision": "bd42b9b45f68a73b454b799a02ca8404",
+    "url": "/resume-website/static/media/video.bd42b9b4.mp4"
+  }
+]);
