2d7e0861fd75f9f848e0c8dfa5331f6fedb0b153
junwei17 <77657709+junwei17@users.noreply.github.com>
2022-12-15 00:24:42 +0800

    updating v1.1

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Icon1 from '../../images/ootb.png';
-import Icon2 from '../../images/uob.png';
-import Icon3 from '../../images/tsi.png';
-import Icon4 from '../../images/tgt.png';
+import Icon2 from '../../images/grab.png';
+// import Icon2 from '../../images/uob.png';
+// import Icon3 from '../../images/tsi.png';
+// import Icon4 from '../../images/tgt.png';
 import Icon5 from '../../images/img1-modified.png';
 import Icon6 from '../../images/img2-modified.png';
 
@@ -16,11 +17,27 @@ import {
   ServicesP
 } from './ServicesElements';
 
+import ProgressBar from "../Progress/progress.js";
+
+
+const testData = [
+  { bgcolor: "#6a1b9a", completed: 60 },
+  { bgcolor: "#00695c", completed: 30 },
+  { bgcolor: "#ef6c00", completed: 53 },
+];
+
 const Services = () => {
   return (
     <ServicesContainer id='services'>
       <ServicesH1>Past Work Experiences</ServicesH1>
       <ServicesWrapper>
+        <ServicesCard>
+          <ServicesIcon src={Icon2} />
+          <ServicesH2>Grab</ServicesH2>
+          <ServicesP>
+            Software Engineer Intern & Full-Stack Developer
+          </ServicesP>
+        </ServicesCard>
         <ServicesCard>
           <ServicesIcon src={Icon5} />
           <ServicesH2>Gifting.sg</ServicesH2>
@@ -42,7 +59,7 @@ const Services = () => {
             Digital Design and Data Analyst Internship
           </ServicesP>
         </ServicesCard>
-        <ServicesCard>
+        {/* <ServicesCard>
           <ServicesIcon src={Icon2} />
           <ServicesH2>UOB</ServicesH2>
           <ServicesP>
@@ -62,8 +79,13 @@ const Services = () => {
           <ServicesP>
             Other Jobs
           </ServicesP>
-        </ServicesCard>
+        </ServicesCard> */}
       </ServicesWrapper>
+      <div style={{width: '60%'}}>
+          {testData.map((item, idx) => (
+              <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
+            ))}
+        </div>
     </ServicesContainer>
   );
 };
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -24,6 +24,7 @@ import {
     Card
   } from './workElements';
 
+
 const Work = () => {
     return (
       <>
